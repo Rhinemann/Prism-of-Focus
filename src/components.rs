bb57529008524067079d448570843d40e3c4e7d5
rhinemann <andreyfrog26@gmail.com>
2026-04-18 23:02:52 +0200

    Added component module

---
+++ src/components.rs
@@ -0,0 +1,28 @@
+use crate::Message;
+use iced::Theme;
+use iced::alignment::Horizontal;
+use iced::border;
+use iced::overlay::menu;
+use iced::widget::{container, text, text_input};
+
+pub fn heading(str: &str) -> text::Text<'_> {
+    text(str).size(18).center()
+}
+
+pub fn centered_column(str: String) -> container::Container<'static, Message> {
+    container(text(str)).align_x(Horizontal::Center)
+}
+
+pub fn combo_box_menu_style(theme: &Theme) -> menu::Style {
+    let mut style = menu::default(theme);
+    style.border.radius = border::Radius::new(15);
+
+    style
+}
+
+pub fn combo_box_input_style(theme: &Theme, status: text_input::Status) -> text_input::Style {
+    let mut style = text_input::default(theme, status);
+    style.border.radius = border::Radius::new(15);
+
+    style
+}
