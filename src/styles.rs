e777cd01485a981edd3fa9a1ce3158529db13235
rhinemann <andreyfrog26@gmail.com>
2026-04-19 11:42:14 +0200

    Replace border::Radius::new() with border::radius()

--- src/styles.rs
+++ src/styles.rs
@@ -7,14 +7,14 @@ const TENET_BOX_BORDER_RADIUS: u32 = 12;
 
 pub fn tenet_box_menu_style(theme: &Theme) -> menu::Style {
     let mut style = menu::default(theme);
-    style.border.radius = border::Radius::new(TENET_BOX_BORDER_RADIUS);
+    style.border.radius = border::radius(TENET_BOX_BORDER_RADIUS);
 
     style
 }
 
 pub fn tenet_box_style(theme: &Theme, status: pick_list::Status) -> pick_list::Style {
     let mut style = pick_list::default(theme, status);
-    style.border.radius = border::Radius::new(TENET_BOX_BORDER_RADIUS);
+    style.border.radius = border::radius(TENET_BOX_BORDER_RADIUS);
 
     style
 }
